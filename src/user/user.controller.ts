4993c1b8504ea9106a75ffae434dbf83dc048168
diti Wade <ditiwade@laposte.net>
2025-01-06 17:08:45 +0100

    first function of app

---
+++ src/user/user.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('users')
+export class UserController {
+constructor(private readonly userService: UserService) {}
+
+    @Get()
+    getUsers() {
+    return this.userService.getUsers();
+    }
+
+
+    @Get(':id')
+    getUser(@Param('id') id: string) {
+        return this.userService.getUser({
+            userId: parseInt(id)
+        });
+    }
+
+
+}
